5da6c08aa7b58d9210ae576dcfdb93d711a17e40
Josua Blejeru <josua@blejeru.com>
2022-12-29 15:14:10 +0200

    💄 text on left, book cover on the right... a lot of Flexbox in between

--- src/components/BookContentCard.tsx
+++ src/components/BookContentCard.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Book } from '../generated/bookshelf-client'
-import { Card, Image, Text } from 'theme-ui'
+import { Card, Image, Text, Flex } from 'theme-ui'
 
 
 
@@ -16,8 +16,10 @@ const BookContentCard = (props: {book: Book, bg: string}) => {
       position: `relative`,
       textDecoration: `none`,
       borderRadius: `lg`,
-      px: 4,
-      py: [4, 5],
+      px: '4rem',
+      py: ['4rem', '5rem'],
+      marginLeft: '1rem',
+      marginRight: '1rem',
       color: `white`,
       background: props.bg || `none`,
       transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
@@ -27,19 +29,25 @@ const BookContentCard = (props: {book: Book, bg: string}) => {
         boxShadow: `xl`,
       },
     }}>
-      <Image src={props.book.imageLinks?.smallThumbnail} />
-      <Text sx={{
-        textTransform: `uppercase`,
-        letterSpacing: `wide`,
-        pt: 2,
-        fontSize: [4, 5],
-        fontWeight: `medium`,
-        lineHeight: 1,
-      }}>{props.book.title}</Text>
-      <Text sx={{
-        fontSize: 2,
-        display: 'block'
-      }}>{props.book.subtitle}</Text>
+        <Flex  sx={{justifyContent: 'center', gap: '1rem'}}>
+        <Image src={props.book.imageLinks?.thumbnail} />
+        <Flex sx={{ flexDirection: 'column', justifyContent: 'center' }}>
+        <Text sx={{
+          textTransform: `uppercase`,
+          letterSpacing: `wide`,
+          pt: 2,
+          fontSize: [4, 5],
+          fontWeight: `medium`,
+          lineHeight: 1,
+        }}>{props.book.title}</Text>
+        <Text sx={{
+          fontSize: 2,
+          display: 'block'
+        }}>
+          {props.book.subtitle}
+        </Text>
+      </Flex>
+      </Flex>
     </Card>
   )
 }
