b72fae48304edc11b8589728624a8eac1f208c86
Josua Blejeru <josua@blejeru.com>
2022-12-29 15:15:06 +0200

    ✨ include a title and a introduction text. Legal disclamer on the bottom

--- src/pages/bookshelves.tsx
+++ src/pages/bookshelves.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { BooksService, BookResponse, Book } from '../generated/bookshelf-client'
 import Layout from '@lekoarts/gatsby-theme-cara/src/components/layout'
 import BookContentCard from '../components/BookContentCard'
+import { Flex, Heading, Text } from 'theme-ui'
 
 const Bookshelves = () => {
   const [books, setBooks] = useState<BookResponse>()
@@ -21,9 +22,26 @@ const Bookshelves = () => {
 
   return (
     <Layout>
-      {books?.response?.map((bookResponse: Book, index: number) => (
-        <BookContentCard book={bookResponse} bg={bg[index % bg.length]}/>
-      ))}
+      <Heading>Bookshelf</Heading>
+      <Text>As an avid reader, I am always on the lookout for books that are informative, thought-provoking, and just plain enjoyable.
+        Over the years, I have come across a number of books that have really stood out to me, and I wanted to share a few of them with you.
+
+        I hope you find these recommendations helpful and that you enjoy the content as much as I did. Happy reading!
+      </Text>
+      
+      <Flex sx={{ flex: '1 1 160px', flexWrap: 'wrap', justifyContent: 'space-between', gap: '1rem'}}>
+        {books?.response?.map((bookResponse: Book, index: number) => (
+          <BookContentCard book={bookResponse} bg={bg[index % bg.length]}/>
+        ))}
+      </Flex>
+
+      <Text>
+        The books and cover art displayed on this website are sourced from various sources, including Google Books. These books and cover art are provided for informational and promotional purposes only, and are not intended for sale. We do not own the rights to these books or cover art, and we are not affiliated with Google Books or the authors of these books.
+
+        By accessing this website, you acknowledge and agree that we are not responsible for any errors or omissions in the information provided, or for any actions you may take based on the information contained on this website.
+
+        Please note that the display of these books and cover art on this website may be subject to copyright and other intellectual property laws. If you are the copyright owner of any of the materials displayed on this website and you believe that your rights have been infringed, please contact us at josua@blejeru.com to request removal of the materials.
+      </Text>
     </Layout>
   )
 }
